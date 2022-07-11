5a664d6509feec981a4dc0c8d68763a7d2d23d86
matheus-s-arruda <matthmsc@gmail.com>
2022-07-10 21:17:27 -0300

    pastas, player, cena 1, resources

---
+++ scripts/player/player_3d.gd
@@ -0,0 +1,34 @@
+extends KinematicBody
+
+const MOVE_SPEED := 6
+const MOUSE_SENSITIVITY := 0.1
+
+onready var camera := $cam_pivot/Camera
+onready var cam_pivot := $cam_pivot
+
+
+var motion : Vector3
+
+
+func _physics_process(_delta):
+	_move_input()
+	
+	motion = move_and_slide(motion, Vector3.UP, true, 4, .785398, false)
+
+
+func _input(event):
+	if event is InputEventKey and Input.is_key_pressed(KEY_ESCAPE):
+		Input.set_mouse_mode((Input.get_mouse_mode() + 2) % 4)
+	
+	if event is InputEventMouseMotion and Input.get_mouse_mode() == Input.MOUSE_MODE_CAPTURED:
+		rotate_y(deg2rad(-event.relative.x * MOUSE_SENSITIVITY))
+		cam_pivot.rotate_x(deg2rad(event.relative.y * MOUSE_SENSITIVITY))
+
+
+func _move_input():
+	var move_direction : Vector3
+	move_direction.z = Input.get_axis("move_down", "move_up")
+	move_direction.x = Input.get_axis("move_right", "move_left")
+	
+	move_direction = (move_direction.x * transform.basis.x) + (move_direction.z * transform.basis.z)
+	motion = lerp(motion, move_direction.normalized() * MOVE_SPEED, 0.25)
