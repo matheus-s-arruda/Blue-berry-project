ac1f2d7f2d7b45ffe39ded1800452afdc9292a1c
BielZinF <71566495+BielZinF@users.noreply.github.com>
2022-07-16 02:54:27 -0300

    ta la

--- scripts/campaing/cena_1b/Amb.tres.gd
+++ scripts/campaing/cena_1b/Amb.tres.gd
@@ -1,8 +1,20 @@
 extends Spatial
 
+var Item_val : int = 0 
+
+onready var VicAnima : AnimationPlayer = $Hud/Vitoria/AnimaVi
+
+func Vit():
+	if Item_val == 3:
+		VicAnima.play("Vit")
+
 onready var gui : CanvasLayer = $gui
 
-func his1():
-	gui.label_alert.emit_alert("Pressione ( ESQ ) para liberar o mouse")
-	gui.dialogue.show_dialogue(PoolStringArray(["kkkkk só capa"]))
 
+
+func _on_Anima_animation_finished(anim_name):
+	get_tree().reload_current_scene()
+
+
+func _on_AnimaVi_animation_finished(anim_name):
+	get_tree().quit()
--- scripts/player/player3D_basics.gd
+++ scripts/player/player3D_basics.gd
@@ -1,6 +1,6 @@
 class_name PlayerBasics3D extends KinematicBody
 
-const GRAVITY := 9.8
+const GRAVITY := 13.8
 
 export(float, 0, 1) var motion_smooth := 0.25
 export var move_speed := 6
@@ -12,12 +12,12 @@ var motion : Vector3
 
 func _physics_process(delta):
 	if enable_gravity:
-		motion.y -= GRAVITY * delta * int(is_on_floor())
+		motion.y -= GRAVITY * delta
 	
 	motion = move_and_slide(motion, Vector3.UP, true, 4, .785398, false)
 
 
 func move_input(move_direction : Vector3):
 	move_direction = (move_direction.x * transform.basis.x) + (move_direction.z * transform.basis.z)
-	motion = lerp(motion, move_direction.normalized() * move_speed, motion_smooth)
-
+	motion.x = lerp(motion.x, move_direction.normalized().x * move_speed, motion_smooth)
+	motion.z = lerp(motion.z, move_direction.normalized().z * move_speed, motion_smooth)
